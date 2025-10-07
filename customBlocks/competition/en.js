c3319b4ec582c399c382598c7a4af524d46b7fee
fustyles <34397289+fustyles@users.noreply.github.com>
2025-10-07 16:29:23 +0800

    Update en.js

--- customBlocks/competition/en.js
+++ customBlocks/competition/en.js
@@ -7,8 +7,9 @@ Blockly.Msg["CATSCRATCH6"] = "Function";
 
 Blockly.Msg["JAVASCRIPT_DATA_INPUT"] = "Input data";
 Blockly.Msg["JAVASCRIPT_DATA_OUTPUT"] = "Output data";
-Blockly.Msg["CATIO"] = "Input/Output";
+Blockly.Msg["CATIO"] = "Event";
 
 Blockly.Msg["JAVASCRIPT_START_HEAD_SCRATCH"] = "When";
 Blockly.Msg["JAVASCRIPT_START_TIE_SCRATCH"] = "Clicked";
+
 Blockly.Msg["JAVASCRIPT_DATA_OUTPUT_SCRATCH"] = "Speak";
