7b3a76b1a0ae882a91e08081c696864303988e1a
fustyles <34397289+fustyles@users.noreply.github.com>
2025-10-07 16:29:01 +0800

    Update zh-hans.js

--- customBlocks/competition/zh-hans.js
+++ customBlocks/competition/zh-hans.js
@@ -7,8 +7,9 @@ Blockly.Msg["CATSCRATCH6"] = "函式";
 
 Blockly.Msg["JAVASCRIPT_DATA_INPUT"] = "输入";
 Blockly.Msg["JAVASCRIPT_DATA_OUTPUT"] = "输出";
-Blockly.Msg["CATIO"] = "功能";
+Blockly.Msg["CATIO"] = "事件";
 
 Blockly.Msg["JAVASCRIPT_START_HEAD_SCRATCH"] = "当";
 Blockly.Msg["JAVASCRIPT_START_TIE_SCRATCH"] = "被点击";
+
 Blockly.Msg["JAVASCRIPT_DATA_OUTPUT_SCRATCH"] = "说出";
