a3e8f183ce41a039734cbc3c1332decabc8c3bc3
fustyles <34397289+fustyles@users.noreply.github.com>
2025-10-31 14:36:37 +0800

    Update en.js

--- customBlocks/competition/en.js
+++ customBlocks/competition/en.js
@@ -30,12 +30,11 @@ Blockly.Msg["TEXT_CHARAT_TITLE_SCRATCH"] = "character %2 of string %1";
 Blockly.Msg["TEXT_CONTAIN_TITLE_SCRATCH"] = "the string %1 contains %2";
 Blockly.Msg["TEXT_LENGTH_TITLE_SCRATCH"] = "length of %1";
 
-Blockly.Msg["NEW_LIST"] = "Make a List";
-
 Blockly.Msg["NEW_VARIABLE_OTHER"] = "Make a Variable";
-Blockly.Msg["VARIABLES_SET_OTHER"] = "Variable %1 is set to %2";
-Blockly.Msg["MATH_CHANGE_TITLE_OTHER"] = "Variable %1 changed to %2";
+Blockly.Msg["VARIABLES_SET_OTHER"] = "variable %1 is set to %2";
+Blockly.Msg["MATH_CHANGE_TITLE_OTHER"] = "variable %1 changed to %2";
 
+Blockly.Msg["NEW_LIST"] = "Make a List";
 Blockly.Msg["LIST_ADDTOLIST"] = "add %1 to %2";
 Blockly.Msg["LIST_ITEMOFLIST"] = "item %2 of %1";
 Blockly.Msg["LIST_DELETEALLOFLIST"] = "delete all of %1";
@@ -45,6 +44,5 @@ Blockly.Msg["LIST_REPLACEITEMOFLIST"] = "replace item %2 of %1 with %3";
 Blockly.Msg["LIST_DELETEOFLIST"] = "delete %2 of %1";
 Blockly.Msg["LIST_LISTCONTAINSITEM"] = "%1 contains %2 ?";
 Blockly.Msg["LIST_ITEMNUMOFLIST"] = "item # of %1 in %2";
-
 
 Blockly.Msg["NEW_FUNCTION"] = "Make a Block";
