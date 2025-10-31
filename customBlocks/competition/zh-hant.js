ca633405cb8bd8915c6b1540ba19cae93d652c9b
fustyles <34397289+fustyles@users.noreply.github.com>
2025-10-31 14:37:12 +0800

    Update zh-hant.js

--- customBlocks/competition/zh-hant.js
+++ customBlocks/competition/zh-hant.js
@@ -30,12 +30,11 @@ Blockly.Msg["TEXT_CHARAT_TITLE_SCRATCH"] = "字串 %1 的第 %2 字";
 Blockly.Msg["TEXT_CONTAIN_TITLE_SCRATCH"] = "字串 %1 包含 %2";
 Blockly.Msg["TEXT_LENGTH_TITLE_SCRATCH"] = "%1 的長度";
 
-Blockly.Msg["NEW_LIST"] = "建立新清單";
-
 Blockly.Msg["NEW_VARIABLE_OTHER"] = "建立變數";
 Blockly.Msg["VARIABLES_SET_OTHER"] = "變數 %1 設為 %2";
 Blockly.Msg["MATH_CHANGE_TITLE_OTHER"] = "變數 %1 改變 %2";
 
+Blockly.Msg["NEW_LIST"] = "建立清單";
 Blockly.Msg["LIST_ADDTOLIST"] = "添加 %1 到 %2";
 Blockly.Msg["LIST_ITEMOFLIST"] = "%1 的第 %2 項";
 Blockly.Msg["LIST_DELETEALLOFLIST"] = "刪除 %1 的所有項目";
