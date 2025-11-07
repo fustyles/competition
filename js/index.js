dafbcf9426da4f4590fd9381e2e79147829050d4
fustyles <34397289+fustyles@users.noreply.github.com>
2025-11-07 09:34:55 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -71,15 +71,17 @@ document.addEventListener('DOMContentLoaded', function() {
 			function onWorkspaceChangedContinuousToolbox(event) {
 				console.log(event);
 				if (event.type=="var_rename"||event.type=="var_delete"||(event.type=="create"&&event.json.type=="javascript_procedures_defnoreturn_scratch")||(event.type=="delete"&&event.oldJson.type=="javascript_procedures_defnoreturn_scratch")) {
-					Blockly.Events.disable();
-					try {
-						if (continuousFlyout.isVisible_ == true) {
-							continuousFlyout.setVisible(false);
-							workspace.toolbox_.clearSelection();
+					setTimeout(function(){
+						Blockly.Events.disable();
+						try {
+							if (continuousFlyout.isVisible_ == true) {
+								continuousFlyout.setVisible(false);
+								workspace.toolbox_.clearSelection();
+							}
+						} finally {
+							Blockly.Events.enable();
 						}
-					} finally {
-						Blockly.Events.enable();
-					}				
+					}, 10);				
 				} else if ((event.type=="create"||event.type=="click")&&continuousFlyout.isVisible_==true) {
 					continuousFlyout.setVisible(false);
 					workspace.toolbox_.clearSelection();
